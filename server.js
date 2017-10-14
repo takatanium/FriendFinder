b84f4415603db962ee1437549e378dbc1d0bd9b5
Tait Takatani <tait.takatani@gmail.com>
2017-10-14 15:40:45 -0700

    added dynamic port

--- server.js
+++ server.js
@@ -2,7 +2,7 @@ let express = require('express');
 let parser = require('body-parser');
 
 let app = express();
-let PORT = 3000;
+let port = process.env.PORT || 3000;
 
 app.use(parser.json());
 app.use(parser.urlencoded({ extended: true }));
@@ -12,6 +12,6 @@ app.use(parser.json({ type: 'application/vnd.api+json' }));
 require("./app/routing/apiRoutes")(app);
 require("./app/routing/htmlRoutes")(app);
 
-app.listen(PORT, function() {
+app.listen(port, function() {
   console.log("App listening on PORT " + PORT);
 });
