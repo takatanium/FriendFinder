4a18998d358739686c718f4daf394ea0a7fc8586
Tait Takatani <tait.takatani@gmail.com>
2017-10-14 15:34:54 -0700

    formatted

--- app/routing/apiRoutes.js
+++ app/routing/apiRoutes.js
@@ -9,19 +9,18 @@ module.exports = function(app) {
   app.post("/api/friends", function(req, res) {
 
     let bestScore, bestFriend;
- 		for (let i = 0; i < friends.length; i++) {
- 			let score = getScore(req.body.scores, friends[i].scores);
- 			if (i === 0) {
- 				bestScore = score;
- 				bestFriend = 0;
- 			}
- 			else {
- 				if (bestScore > score) {
- 					bestScore = score;
- 					bestFriend = i;
- 				}
- 			}
- 		}
+    for (let i = 0; i < friends.length; i++) {
+      let score = getScore(req.body.scores, friends[i].scores);
+      if (i === 0) {
+        bestScore = score;
+        bestFriend = 0;
+      } else {
+        if (bestScore > score) {
+          bestScore = score;
+          bestFriend = i;
+        }
+      }
+    }
 
     friends.push(req.body);
     res.json(friends[bestFriend]);
@@ -30,12 +29,12 @@ module.exports = function(app) {
 };
 
 let getScore = function (fr1, fr2) {
-	let score = 0;
+  let score = 0;
 
-	for (let i = 0; i < fr1.length; i++) {
-		let diff = Math.abs(parseInt(fr1[i]) - parseInt(fr2[i]));
-		score += diff;
-	}
+  for (let i = 0; i < fr1.length; i++) {
+    let diff = Math.abs(parseInt(fr1[i]) - parseInt(fr2[i]));
+    score += diff;
+  }
 
-	return score;
+  return score;
 };
